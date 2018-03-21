8360f6454d186bf02eeff8710f7015b65390593f
Kapu <ravi.kapu@delta.com>
2018-03-21 09:52:42 -0400

    SpringBoot Application

---
+++ src/main/java/com/delta/webservices/springbootsample/HelloController.java
@@ -0,0 +1,17 @@
+package com.delta.webservices.springbootsample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+}
